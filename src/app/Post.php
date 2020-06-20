6c44c2bffcf767f39e1caa2951191ba4c3bbfdfc
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2020-06-20 11:14:09 +0900

    [feat]lighthouseのsubscriptionを実装

--- src/app/Post.php
+++ src/app/Post.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    protected $fillable = ['title', 'content'];
+
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
